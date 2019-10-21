41ca21007be3f2ca155d75c1ee045d04253c01ed
imanw <imanw77@gmail.com>
2019-10-21 17:57:11 +0330

    Re-design the webpage

--- src/App.vue
+++ src/App.vue
@@ -1,82 +1,79 @@
 <template>
   <div id="app">
     <div class="container">
-      <section class="col-row">
-        <div class="search justify-content-md-center">
-          <h2>SELECT A COUNTRY</h2>
-          <select name="country" id="reg_select" v-model="selectedCountry" @change="getWeatherData">
-            <option v-for="country in countriesNames" :value="country">{{ country }}</option>
-          </select>
+      <div class="search">
+        <h2>SELECT A COUNTRY</h2>
+        <select name="country" id="reg_select" v-model="selectedCountry" @change="getWeatherData">
+          <option v-for="country in countriesNames" :value="country">{{ country }}</option>
+        </select>
+      </div>
+      <div class="boxs">
+        <div class="country-detail shodow">
+          <h2>Country Name {{ currentCountry.name }}</h2>
+          <p>
+            Native Name:
+            <span>{{ currentCountry.nativeName }}</span>
+          </p>
+          <p>
+            Capital:
+            <span>{{ currentCountry.capital }}</span>
+          </p>
+          <p>
+            Region:
+            <span>{{ currentCountry.region }} {{ currentCountry.subregion }}</span>
+          </p>
+          <p>
+            Population:
+            <span>{{ currentCountry.population }}</span>
+          </p>
+          <p>
+            Languages:
+            <span
+              v-for="lang in currentCountry.languages"
+            >{{ lang.name }}, {{ lang.nativeName }}</span>
+          </p>
+          <p>
+            Timezones:
+            <span v-for="zone in currentCountry.timezones">{{ zone }}</span>
+          </p>
         </div>
-        <div class="boxs row justify-content-md-center">
-          <div class="details col-lg-4 col-md-12" id="details">
-            <h3>{{ currentCountry.name }}</h3>
-            <p>
-              Native Name:
-              <span>{{ currentCountry.nativeName }}</span>
-            </p>
-            <p>
-              Capital:
-              <span>{{ currentCountry.capital }}</span>
-            </p>
-            <p>
-              Region:
-              <span>{{ currentCountry.region }} {{ currentCountry.subregion }}</span>
-            </p>
-            <p>
-              Population:
-              <span>{{ currentCountry.population }}</span>
-            </p>
-            <p>
-              Languages:
-              <span
-                v-for="lang in currentCountry.languages"
-              >{{ lang.name }}, {{ lang.nativeName }}</span>
-            </p>
-            <p>
-              Timezones:
-              <span v-for="zone in currentCountry.timezones">{{ zone }}</span>
-            </p>
-          </div>
-          <div class="call-code col-lg-4 col-md-12">
-            <h2>CALLING CODE</h2>
-            <h1 v-for="callCode in currentCountry.callingCodes">{{ callCode }}</h1>
-          </div>
-          <div class="flag col-lg-4 col-md-12">
-            <img :src="currentCountry.flag" />
-          </div>
-          <div class="container row justify-content-md-center">
-            <div class="weather col-lg-6 col-md-12">
-              <h2>CAPITAL WEATHER REPORT</h2>
-              <img
-                v-if="checkObject(currentWeather)"
-                :src="'http://openweathermap.org/img/w/'+currentWeather.weather[0].icon+'.png'"
-              />
-              <p>
-                Wind Speed:
-                <span v-if="checkObject(currentWeather)">{{ currentWeather.wind.speed }}</span>ms
-              </p>
-              <p>
-                Temprerature:
-                <span v-if="checkObject(currentWeather)">{{ currentWeather.main.temp }}</span>c
-              </p>
-              <p>
-                Humidity:
-                <span v-if="checkObject(currentWeather)">{{ currentWeather.main.humidity }}</span>%
-              </p>
-              <p>
-                Visibility:
-                <span v-if="checkObject(currentWeather)">{{ currentWeather.visibility }}</span>m
-              </p>
-            </div>
-            <div id="map" v-if="checkObject(currentWeather)" class="col-lg-6 col-md-12">
-              <l-map style="height: 100%;" :zoom="map.zoom" :center="map.center">
-                <l-tile-layer :url="map.url" :attribution="map.attribution"></l-tile-layer>
-              </l-map>
-            </div>
-          </div>
+        <div class="call-code shodow">
+          <h2>CALLING CODE</h2>
+          <h1 v-for="callCode in currentCountry.callingCodes">{{ callCode }}</h1>
         </div>
-      </section>
+        <div class="flag shodow">
+          <img :src="currentCountry.flag" />
+        </div>
+        <div class="weather shodow">
+          <h2>CAPITAL WEATHER REPORT</h2>
+          <img
+          class="weather_status"
+            v-if="checkObject(currentWeather)"
+            :src="'http://openweathermap.org/img/w/'+currentWeather.weather[0].icon+'.png'"
+          />
+          <p>
+            <img src="./assets/wind.png"> Wind Speed:
+            <span v-if="checkObject(currentWeather)">{{ currentWeather.wind.speed }}</span>ms
+          </p>
+          <p>
+            <img src="./assets/temp.png"> Temprerature:
+            <span v-if="checkObject(currentWeather)">{{ currentWeather.main.temp }}</span>c
+          </p>
+          <p>
+            <img src="./assets/humidity.png"> Humidity:
+            <span v-if="checkObject(currentWeather)">{{ currentWeather.main.humidity }}</span>%
+          </p>
+          <p>
+            <img src="./assets/visibility.png"> Visibility:
+            <span v-if="checkObject(currentWeather)">{{ currentWeather.visibility }}</span>m
+          </p>
+        </div>
+        <div id="map" class="shadow">
+          <l-map v-if="checkObject(currentWeather)"  :zoom="map.zoom" :center="map.center">
+            <l-tile-layer :url="map.url" :attribution="map.attribution"></l-tile-layer>
+          </l-map>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -156,5 +153,5 @@ export default {
 <style>
 @import "./assets/style.css";
 @import "https://unpkg.com/leaflet@1.5.1/dist/leaflet.css";
-@import "./assets/bootstrap.min.css";
+/* @import "./assets/bootstrap.min.css"; */
 </style>
